8c9fc5ea02bcba329a428461586abafe0b4a3236
Mohammad Bin Sultan <mohammadfcc2745@gmail.com>
2021-08-01 11:57:13 +0600

    salary updated

--- script/management/generic/salary.php
+++ script/management/generic/salary.php
@@ -25,16 +25,19 @@ $EM->InputValidation([
 $EM->ValidateInput(function($Entity, $Database, $Table, $PrimaryKey, $ID){
 	$Result = true;
 
-	// if($Table->Get( // Check for duplicate values for UNIQUE columns
-	// 	//Check same person have same address
-	// 	"
-	// 		(
-	// 				{$Table->Alias()}." . ($Column = "PersonID") . " = " . intval($_POST["{$Column}"]) . "
-	// 			AND	" . ($Column = "{$Entity}Name") . " = '{$Database->Escape($_POST["{$Column}"])}'
-	// 		)
-	// 		AND	{$PrimaryKey} <> {$ID}
-	// 	"
-	// , null, null, null, null, null, null))$Result = "Same person and address name for the same " . strtolower($Table->FormalName()) . " exists!";
+	$SalaryMonth = date('m', strtotime($_POST["EmployeeSalaryDate"]));
+	$SalaryYear = date('Y', strtotime($_POST["EmployeeSalaryDate"]));
+
+	if($Table->Get( // Check for duplicate values for UNIQUE columns
+		//Check same person have same address
+		"
+			(	
+				{$Table->Alias()}." . ($Column = "EmployeeID") . " = " . intval($_POST["{$Column}"]) . "
+				AND	MONTH (" . ($Column = "{$Entity}Date") . ") = " . $SalaryMonth . "
+				AND	YEAR (" . ($Column = "{$Entity}Date") . ") = " . $SalaryYear . "
+			)
+		"
+	, null, null, null, null, null, null))$Result = "This employee salary has been already paid !";
 
 	return $Result;
 });
@@ -77,7 +80,7 @@ $EM->BatchActionHTML([
 ]);
 
 # Configuration Section
-$EM->OrderBy("EmployeeName ASC, {$Entity}ID"); # Order by fields
+$EM->OrderBy("EmployeeName"); # Order by fields
 $EM->Order("ASC"); # Return record ASC / DESC
 $EM->URL($Application->URL($_POST["_Script"])); # Current script URL
 $EM->IconURL($Environment->IconURL()); # Icon URL
@@ -118,8 +121,16 @@ if(isset($_POST["btnInput"])){
 	$NewRecordMode = isset($_POST["{$Entity}ID"]) && intval($_POST["{$Entity}ID"]) ? false : true;
 
 	if(isset($_POST["btnSubmit"])){
-		#region Custom code
 		#endregion Custom code
+		// $_POST["EmployeeSalaryDate"] = date("Y-m-d");
+		$_POST["EmployeeSalaryHousing"] = ($_POST['EmployeeSalaryBasic'] * 15) / 100;
+		$_POST["EmployeeSalaryTransport"] = ($_POST['EmployeeSalaryBasic'] * 10) / 100;
+		$_POST["EmployeeSalaryMedical"] = ($_POST['EmployeeSalaryBasic'] * 5) / 100;
+		$_POST["EmployeeSalaryPf"] = ($_POST['EmployeeSalaryBasic'] * 0) / 100;
+		$_POST["EmployeeSalaryFood"] = ($_POST['EmployeeSalaryBasic'] * 5) / 100;
+		$_POST["EmployeeSalaryTax"] = ($_POST['EmployeeSalaryBasic'] * 0) / 100;
+		#region Custom code
+		// debugdump($_POST);
 
 		if($EM->Input()){
 			$Terminal->Redirect("{$_POST["_Referer"]}&SucceededAction=Input"); // Redirect to previous location
@@ -132,9 +143,10 @@ if(isset($_POST["btnInput"])){
 
 	# Input Form Section
 	$EM->InputUIHTML([
-		HTML\UI\Field(HTML\UI\Select("" . ($Caption = "Employee") . "ID", $Table[$OptionEntity = "{$Caption}"]->Get("{$Table["{$OptionEntity}"]->Alias()}.{$OptionEntity}IsActive = 1", "{$OptionEntity}LookupCaption ASC"), null, "{$OptionEntity}LookupCaption"), "{$Caption}", null, null, $EM->FieldCaptionWidth()),
-		HTML\UI\Field(HTML\UI\Input("{$Entity}" . ($Caption = "Basic") . "", $EM->InputWidth(), null, true), "{$Caption}", true, null, $EM->FieldCaptionWidth()),
-		HTML\UI\Field(HTML\UI\Input("{$Entity}" . ($Caption = "Total") . "", $EM->InputWidth(), null, null), "{$Caption}", true, null, $EM->FieldCaptionWidth()),
+		HTML\UI\Field(HTML\UI\Select("" . ($Caption = "Employee") . "ID", $Table[$OptionEntity = "{$Caption}"]->Get("{$Table["{$OptionEntity}"]->Alias()}.{$OptionEntity}IsActive = 1", "{$OptionEntity}LookupCaption ASC"), new option(), "{$OptionEntity}LookupCaption"), "{$Caption}", null, null, $EM->FieldCaptionWidth()),
+		HTML\UI\Field(HTML\UI\Input("{$Entity}" . ($Caption = "Date") . "", $EM->InputWidth(), DATE("Y-m-d"), null, INPUT_TYPE_DATE), "{$Caption}", true, null, $EM->FieldCaptionWidth()),
+		HTML\UI\Field(HTML\UI\Input("{$Entity}" . ($Caption = "Basic") . "", $EM->InputWidth(), null, true, null, null, null, ["OnKeyUp" => "salarycalc(this);"], "{$Entity}Basic" ), "{$Caption}", true, null, $EM->FieldCaptionWidth()),
+		HTML\UI\Field(HTML\UI\Input("{$Entity}" . ($Caption = "Total") . "Amount", $EM->InputWidth(), null, null, null, null, null, null, "{$Entity}TotalAmount", null, null, null, true), "{$Caption}", true, null, $EM->FieldCaptionWidth()),
 		HTML\UI\Field(HTML\UI\RadioGroup("{$Entity}" . ($Caption = "Status") . "", [new HTML\UI\Radio("Paid", "Paid"), new HTML\UI\Radio("Unpaid", "Unpaid")]), "{$Caption}", true, null, $EM->FieldCaptionWidth()),
 	]);
 
@@ -172,3 +184,19 @@ print "{$EM->ListHTML()}";
 if(SetVariable("SucceededAction") == "Input")print HTML\UI\Toast("{$Table["{$Entity}"]->FormalName()} input successful.");
 #region List
 ?>
+
+<script>
+	function salarycalc(basic){
+		BASIC = parseInt(basic.value);
+		HA = (BASIC * 15) / 100;
+		TA = (BASIC * 10) / 100;
+		MA = (BASIC * 5) / 100;
+		PA = (BASIC * 0) / 100;
+		FA = (BASIC * 5) / 100;
+		OTHER = (BASIC * 0) / 100;
+		TAX = (BASIC * 0) / 100;
+		VAT = (BASIC * 0) / 100;
+		TOTAL = BASIC + HA + TA + MA + PA - FA + OTHER - TAX - VAT;
+		document.getElementById("EmployeeSalaryTotalAmount").value = TOTAL ;
+	}
+</script>
